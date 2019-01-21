984cdccba92a070d0d875673b96cd535e97c5631
Ben Snow <roskoe432@gmail.com>
2019-01-20 20:41:55 -0500

    Created role class

---
+++ src/models/role.ts
@@ -0,0 +1,20 @@
+export class Role {
+    private static count: number = 0;
+    
+    private _roleId: number; // primary key
+    private _role: string; // not null, unique
+
+    constructor(role: string) {
+        this._roleId = Role.count;
+        this._role = role;
+
+        Role.count++;
+        console.log(this._roleId);
+    }
+
+    get getRoleId(): number { return this._roleId; }
+    get getRole(): string { return this._role; }
+}
+
+let a = new Role("cook");
+let b = new Role("plumber");
