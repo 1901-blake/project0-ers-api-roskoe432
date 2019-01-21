e980faccf326a5096129acf8814b92b55b1f9dcf
Ben Snow <roskoe432@gmail.com>
2019-01-20 20:35:44 -0500

    Added properties and test code to user model

--- src/models/user.ts
+++ src/models/user.ts
@@ -1,25 +1,36 @@
 export class User {
-    private static count: number;
+    private static count: number = 0;
 
-    private userId: number;
-    public username: string;
-    public password: string;
-    public firstName: string;
-    public lastName: string;
-    public email: string;
-    // Role class
+    private _userId: number; // Primary Key
+    public username: string; // not null, unique
+    public password: string; // not null
+    public firstName: string; // not null
+    public lastName: string; // not null
+    public email: string; // not null
+    // Role class // not null
 
     constructor(username: string, email: string, password: string, firstName: string, lastName: string) {
-        User.count++;
-        this.userId = User.count;
+        this._userId = User.count;
         this.username = username;
         this.email = email;
         this.password = password;
         this.firstName = firstName;
         this.lastName = lastName;
+
+        User.count++;
+        console.log(this._userId);
     } 
 
-    public getUserId() { return this.userId; }
+    get getUserId(): number { return this._userId; }
+    get getFullName(): string {
+        return `${this.firstName} ${this.lastName}`
+    }
 }
 
-console.log("");
+let a = new User('bill', 'bill@gmail.com', 'pw', 'bill', 'bo');
+let b = new User('bob', 'bob@yahoo.com', 'pw', 'bob', 'by');
+let c = new User('john', 'who@gmail.com', 'pw', 'john', 'doe');
+
+console.log(a.getFullName);
+console.log(b.getFullName);
+console.log(c.getFullName);
